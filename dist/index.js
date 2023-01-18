84a65572e036e7ad69e138f51bd7b83f68d48f3e
Mark_Maistrenko <revanmayfall@gmail.com>
2023-01-18 20:53:56 +0400

    +get +post + get id+put + delete for posts

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const jsonBodyMiddleware = express_1.default.json();
 app.use(jsonBodyMiddleware);
 let bd = {
     blogs: [],
-    post: [],
+    posts: [],
 };
 let errorsMessages = [];
 function message(a, b) {
@@ -185,6 +185,220 @@ app.delete("/blogs/:id", (req, res) => {
     let oneBlog = bd.blogs.find((p) => p.id === id);
     if (oneBlog) {
         bd.blogs = bd.blogs.filter((p) => p.id !== id);
+        res.send(204);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.get("/posts", (req, res) => {
+    res.status(200).json(bd.posts);
+});
+app.post("/posts", (req, res) => {
+    errorsMessages = [];
+    let isId = "";
+    if (bd.posts.length === 0) {
+        isId = "0";
+    }
+    else if (bd.posts.length === 1) {
+        if (bd.posts[0].id !== "0") {
+            isId = "0";
+        }
+        else {
+            isId = "1";
+        }
+    }
+    else {
+        for (let i = 1; i < bd.posts.length; i++) {
+            let elementTwo = bd.posts[i - 1];
+            let idTwo = Number(elementTwo.id);
+            let elementOne = bd.posts[i];
+            let idOne = Number(elementOne.id);
+            let raznitsaId = idOne - idTwo;
+            if (raznitsaId !== 1) {
+                isId = String(idOne + 1);
+                break;
+            }
+            if (i === bd.posts.length - 1) {
+                isId = String(idOne + 1);
+                break;
+            }
+        }
+    }
+    let isTitle = "";
+    if (!req.body.title) {
+        message("Write title", "title");
+    }
+    else if (typeof req.body.title !== "string") {
+        message("Please write string", "title");
+    }
+    else if (req.body.title.length > 30) {
+        message("Write title less 30 letters", "title");
+    }
+    else {
+        isTitle = req.body.title;
+    }
+    let isShortDescription = "";
+    if (!req.body.shortDescription) {
+        message("Write shortDescription", "shortDescription");
+    }
+    else if (typeof req.body.shortDescription !== "string") {
+        message("Please write string", "shortDescription");
+    }
+    else if (req.body.shortDescription.length > 100) {
+        message("Write shortDescription less 100 letters", "shortDescription");
+    }
+    else {
+        isShortDescription = req.body.shortDescription;
+    }
+    let isContent = "";
+    if (!req.body.content) {
+        message("Write content", "content");
+    }
+    else if (typeof req.body.content !== "string") {
+        message("Please write content like string", "content");
+    }
+    else if (req.body.content.length > 1000) {
+        message("Write content less 1000 letters", "content");
+    }
+    else {
+        isContent = req.body.content;
+    }
+    let g = false;
+    let isBlogName = "";
+    for (let aa = 0; aa < bd.blogs.length; aa++) {
+        if (req.body.blogId === bd.blogs[aa].id) {
+            g = true;
+            isBlogName = bd.blogs[aa].name;
+            break;
+        }
+    }
+    let isBlogId = "";
+    if (!req.body.blogId) {
+        message("Write blogId", "blogId");
+    }
+    else if (typeof req.body.blogId !== "string") {
+        message("Please write blogId like string", "blogId");
+    }
+    else if (g === false) {
+        message("Please insert existed user id", "blogId");
+    }
+    else {
+        isBlogId = req.body.blogId;
+    }
+    if (errorsMessages.length > 0) {
+        res.status(400).json({ errorsMessages: errorsMessages });
+    }
+    else {
+        const createPost = {
+            id: isId,
+            title: isTitle,
+            shortDescription: isShortDescription,
+            content: isContent,
+            blogId: isBlogId,
+            blogName: isBlogName,
+        };
+        bd.posts.push(createPost);
+        res.status(201).json(createPost);
+    }
+});
+app.get("/posts/:id", (req, res) => {
+    let id = req.params.id;
+    let onePost = bd.posts.find((p) => p.id === id);
+    if (onePost) {
+        res.status(200).json(onePost);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.put("/posts/:id", (req, res) => {
+    let id = req.params.id;
+    let onePost = bd.posts.find((p) => p.id === id);
+    if (onePost) {
+        errorsMessages = [];
+        let isTitle = "";
+        if (!req.body.title) {
+            message("Write title", "title");
+        }
+        else if (typeof req.body.title !== "string") {
+            message("Please write string", "title");
+        }
+        else if (req.body.title.length > 30) {
+            message("Write title less 30 letters", "title");
+        }
+        else {
+            isTitle = req.body.title;
+        }
+        let isShortDescription = "";
+        if (!req.body.shortDescription) {
+            message("Write shortDescription", "shortDescription");
+        }
+        else if (typeof req.body.shortDescription !== "string") {
+            message("Please write string", "shortDescription");
+        }
+        else if (req.body.shortDescription.length > 100) {
+            message("Write shortDescription less 100 letters", "shortDescription");
+        }
+        else {
+            isShortDescription = req.body.shortDescription;
+        }
+        let isContent = "";
+        if (!req.body.content) {
+            message("Write content", "content");
+        }
+        else if (typeof req.body.content !== "string") {
+            message("Please write content like string", "content");
+        }
+        else if (req.body.content.length > 1000) {
+            message("Write content less 1000 letters", "content");
+        }
+        else {
+            isContent = req.body.content;
+        }
+        let g = false;
+        let isBlogName = "";
+        for (let aa = 0; aa < bd.blogs.length; aa++) {
+            if (req.body.blogId === bd.blogs[aa].id) {
+                g = true;
+                isBlogName = bd.blogs[aa].name;
+                break;
+            }
+        }
+        let isBlogId = "";
+        if (!req.body.blogId) {
+            message("Write blogId", "blogId");
+        }
+        else if (typeof req.body.blogId !== "string") {
+            message("Please write blogId like string", "blogId");
+        }
+        else if (g === false) {
+            message("Please insert existed user id", "blogId");
+        }
+        else {
+            isBlogId = req.body.blogId;
+        }
+        if (errorsMessages.length > 0) {
+            res.status(400).json({ errorsMessages: errorsMessages });
+        }
+        else {
+            (onePost.title = isTitle),
+                (onePost.shortDescription = isShortDescription),
+                (onePost.content = isContent),
+                (onePost.blogId = isBlogId),
+                (onePost.blogName = isBlogName),
+                res.send(204);
+        }
+    }
+    else {
+        res.send(404);
+    }
+});
+app.delete("/posts/:id", (req, res) => {
+    let id = req.params.id;
+    let onePost = bd.posts.find((p) => p.id === id);
+    if (onePost) {
+        bd.posts = bd.posts.filter((p) => p.id !== id);
         res.send(204);
     }
     else {
--- src/index.ts
+++ src/index.ts
@@ -36,9 +36,9 @@ type PostViewModel = {
   blogName: string;
 };
 
-let bd: { blogs: BlogViewModel[]; post: PostViewModel[] } = {
+let bd: { blogs: BlogViewModel[]; posts: PostViewModel[] } = {
   blogs: [],
-  post: [],
+  posts: [],
 };
 
 let errorsMessages: ApiErrorResult = [];
@@ -203,6 +203,209 @@ app.delete("/blogs/:id", (req: Request, res: Response) => {
   let oneBlog = bd.blogs.find((p) => p.id === id);
   if (oneBlog) {
     bd.blogs = bd.blogs.filter((p) => p.id !== id);
+    res.send(204);
+  } else {
+    res.send(404);
+  }
+});
+
+app.get("/posts", (req: Request, res: Response) => {
+  res.status(200).json(bd.posts);
+});
+
+app.post("/posts", (req: Request<{}, {}, PostInputModel>, res: Response) => {
+  errorsMessages = [];
+
+  let isId: string = "";
+  if (bd.posts.length === 0) {
+    isId = "0";
+  } else if (bd.posts.length === 1) {
+    if (bd.posts[0].id !== "0") {
+      isId = "0";
+    } else {
+      isId = "1";
+    }
+  } else {
+    for (let i = 1; i < bd.posts.length; i++) {
+      let elementTwo = bd.posts[i - 1];
+      let idTwo = Number(elementTwo.id);
+      let elementOne = bd.posts[i];
+      let idOne = Number(elementOne.id);
+      let raznitsaId = idOne - idTwo;
+      if (raznitsaId !== 1) {
+        isId = String(idOne + 1);
+        break;
+      }
+      if (i === bd.posts.length - 1) {
+        isId = String(idOne + 1);
+        break;
+      }
+    }
+  }
+
+  let isTitle: string = "";
+  if (!req.body.title) {
+    message("Write title", "title");
+  } else if (typeof req.body.title !== "string") {
+    message("Please write string", "title");
+  } else if (req.body.title.length > 30) {
+    message("Write title less 30 letters", "title");
+  } else {
+    isTitle = req.body.title;
+  }
+
+  let isShortDescription: string = "";
+  if (!req.body.shortDescription) {
+    message("Write shortDescription", "shortDescription");
+  } else if (typeof req.body.shortDescription !== "string") {
+    message("Please write string", "shortDescription");
+  } else if (req.body.shortDescription.length > 100) {
+    message("Write shortDescription less 100 letters", "shortDescription");
+  } else {
+    isShortDescription = req.body.shortDescription;
+  }
+
+  let isContent: string = "";
+  if (!req.body.content) {
+    message("Write content", "content");
+  } else if (typeof req.body.content !== "string") {
+    message("Please write content like string", "content");
+  } else if (req.body.content.length > 1000) {
+    message("Write content less 1000 letters", "content");
+  } else {
+    isContent = req.body.content;
+  }
+  let g: boolean = false;
+  let isBlogName: string = "";
+
+  for (let aa = 0; aa < bd.blogs.length; aa++) {
+    if (req.body.blogId === bd.blogs[aa].id) {
+      g = true;
+      isBlogName = bd.blogs[aa].name;
+      break;
+    }
+  }
+
+  let isBlogId: string = "";
+  if (!req.body.blogId) {
+    message("Write blogId", "blogId");
+  } else if (typeof req.body.blogId !== "string") {
+    message("Please write blogId like string", "blogId");
+  } else if (g === false) {
+    message("Please insert existed user id", "blogId");
+  } else {
+    isBlogId = req.body.blogId;
+  }
+
+  if (errorsMessages.length > 0) {
+    res.status(400).json({ errorsMessages: errorsMessages });
+  } else {
+    const createPost: PostViewModel = {
+      id: isId,
+      title: isTitle,
+      shortDescription: isShortDescription,
+      content: isContent,
+      blogId: isBlogId,
+      blogName: isBlogName,
+    };
+    bd.posts.push(createPost);
+    res.status(201).json(createPost);
+  }
+});
+
+app.get("/posts/:id", (req: Request, res: Response) => {
+  let id: string = req.params.id;
+  let onePost = bd.posts.find((p) => p.id === id);
+  if (onePost) {
+    res.status(200).json(onePost);
+  } else {
+    res.send(404);
+  }
+});
+
+app.put(
+  "/posts/:id",
+  (req: Request<{ id: string }, {}, PostInputModel>, res: Response) => {
+    let id: string = req.params.id;
+    let onePost = bd.posts.find((p) => p.id === id);
+    if (onePost) {
+      errorsMessages = [];
+
+      let isTitle: string = "";
+      if (!req.body.title) {
+        message("Write title", "title");
+      } else if (typeof req.body.title !== "string") {
+        message("Please write string", "title");
+      } else if (req.body.title.length > 30) {
+        message("Write title less 30 letters", "title");
+      } else {
+        isTitle = req.body.title;
+      }
+
+      let isShortDescription: string = "";
+      if (!req.body.shortDescription) {
+        message("Write shortDescription", "shortDescription");
+      } else if (typeof req.body.shortDescription !== "string") {
+        message("Please write string", "shortDescription");
+      } else if (req.body.shortDescription.length > 100) {
+        message("Write shortDescription less 100 letters", "shortDescription");
+      } else {
+        isShortDescription = req.body.shortDescription;
+      }
+
+      let isContent: string = "";
+      if (!req.body.content) {
+        message("Write content", "content");
+      } else if (typeof req.body.content !== "string") {
+        message("Please write content like string", "content");
+      } else if (req.body.content.length > 1000) {
+        message("Write content less 1000 letters", "content");
+      } else {
+        isContent = req.body.content;
+      }
+      let g: boolean = false;
+      let isBlogName: string = "";
+
+      for (let aa = 0; aa < bd.blogs.length; aa++) {
+        if (req.body.blogId === bd.blogs[aa].id) {
+          g = true;
+          isBlogName = bd.blogs[aa].name;
+          break;
+        }
+      }
+
+      let isBlogId: string = "";
+      if (!req.body.blogId) {
+        message("Write blogId", "blogId");
+      } else if (typeof req.body.blogId !== "string") {
+        message("Please write blogId like string", "blogId");
+      } else if (g === false) {
+        message("Please insert existed user id", "blogId");
+      } else {
+        isBlogId = req.body.blogId;
+      }
+
+      if (errorsMessages.length > 0) {
+        res.status(400).json({ errorsMessages: errorsMessages });
+      } else {
+        (onePost.title = isTitle),
+          (onePost.shortDescription = isShortDescription),
+          (onePost.content = isContent),
+          (onePost.blogId = isBlogId),
+          (onePost.blogName = isBlogName),
+          res.send(204);
+      }
+    } else {
+      res.send(404);
+    }
+  }
+);
+
+app.delete("/posts/:id", (req: Request, res: Response) => {
+  let id: string = req.params.id;
+  let onePost = bd.posts.find((p) => p.id === id);
+  if (onePost) {
+    bd.posts = bd.posts.filter((p) => p.id !== id);
     res.send(204);
   } else {
     res.send(404);
